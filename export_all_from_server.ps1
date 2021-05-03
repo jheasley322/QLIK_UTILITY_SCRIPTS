51c876668884a7ca3137c60330e52cc028f022f2
Joe Easley <40567905+jheasley322@users.noreply.github.com>
2021-05-03 07:41:05 -0400

    Create export_all_from_server.ps1

---
+++ export_all_from_server.ps1
@@ -0,0 +1,19 @@
+Get-PfxCertificate "C:\Users\JoeEasley\OneDrive - Odyssey Logistics & Technology Corporation\Desktop\NC-JEASLY1\client.pfx" | Connect-Qlik -computername t1prd-qlik01.odysseylogistics.COM -username odysseylogistic\QlikServiceProd -TrustAllCerts -verbose
+$myserver = "t1prd-qlik01.odysseylogistics.COM"
+$folder = "C:\Users\JoeEasley\OneDrive - Odyssey Logistics & Technology Corporation\Desktop\Archive_Unused_5_3_21"
+$streamname = "UTIL - Out of Service"
+
+Connect-Qlik $myserver ## check https://github.com/ahaydon/Qlik-Cli for details
+
+foreach($qvf in $(Get-QlikApp)) {
+    if ($qvf.published -and $qvf.stream.name) { # Is it published?
+        $streamfolder = $qvf.stream.name
+        If(!(test-path "$folder\$streamfolder")) # Create a folder if it does not exists
+        {
+              New-Item -ItemType Directory -Force -Path "$folder\$streamfolder"
+        }
+    } else {
+        $streamfolder = ""
+    }
+    Export-QlikApp -id $qvf.id -filename "$($folder)\$($streamfolder)\$($qvf.name).qvf" #dumps the qvf
+}
