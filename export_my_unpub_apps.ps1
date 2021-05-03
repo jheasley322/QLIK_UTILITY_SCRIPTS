fbe97b629e22a6432aa530d3c8c50f83d54bcaab
Joe Easley <40567905+jheasley322@users.noreply.github.com>
2021-05-03 11:10:00 -0400

    Update export_my_unpub_apps.ps1
    added more variables, a list of apps to be exported, and details of the currently exported app.

--- export_my_unpub_apps.ps1
+++ export_my_unpub_apps.ps1
@@ -1,19 +1,37 @@
-Get-PfxCertificate "C:\Users\JoeEasley\OneDrive - Odyssey Logistics & Technology Corporation\Desktop\NC-JEASLY1\client.pfx" | Connect-Qlik -computername t1prd-qlik01.odysseylogistics.COM -username odysseylogistic\QlikServiceProd -TrustAllCerts -verbose
-$myserver = "t1prd-qlik01.odysseylogistics.COM"
-$folder = "C:\Users\JoeEasley\OneDrive - Odyssey Logistics & Technology Corporation\Desktop\Archive_Unused_5_3_21"
+$certloc = "C:\Users\JoeEasley\OneDrive - Odyssey Logistics & Technology Corporation\Desktop\NC-JEASLY1\client.pfx"
+$myserver = "t1prd-qlik01.odysseylogistics.com"
+$archive = "Archive_Unused_5_3_21"
+$folder = "C:\Users\JoeEasley\OneDrive - Odyssey Logistics & Technology Corporation\Desktop\$archive"
 $username = "Joe Easley (NC)"
+$domainname = "ODYSSEYLOGISTIC"
+$serviceacct = "QlikServiceProd"
 
-Get-QlikApp -filter "owner.name eq '$username' and Published eq False"
-## Get-QlikApp -filter "published eq False"
-foreach($qvf in $(Get-QlikApp -filter "owner.name eq '$username' and Published eq False"))
-    {
-    If(!(test-path "$folder\$username")) # Create a folder if it does not exists
-        {
-            New-Item -ItemType Directory -Force -Path "$folder\$username"
-        }
-    else 
-        {
-            $username = ""
-        }
-    Export-QlikApp -id $qvf.id -filename "$($folder)\$($username)\$($qvf.name).qvf" #dumps the qvf
+
+Get-PfxCertificate "$certloc" | Connect-Qlik -computername "$myserver" -username "$domainname\$serviceacct" -TrustAllCerts -verbose
+
+##show list of apps to be exported
+[System.Collections.ArrayList]$apps = @()
+foreach($qvf in $(Get-QlikApp -filter "owner.name eq '$username' and Published eq False" -full))
+{
+    $s = [PSCustomObject]@{
+        Name                = $qvf.name 
+        Fileize_kb          = $qvf.Filesize/1000
+        Reloaded            = $qvf.lastReloadTime
+    }
+    $apps.Add($s)
 }
+$apps | Format-Table
+
+##export each app to the specified directory
+foreach($qvf in $(Get-QlikApp -filter "owner.name eq '$username' and Published eq False" -full))
+{
+    New-Item -ItemType Directory -Force -Path "$folder\$username"
+    $s = [PSCustomObject]@{
+        Name                = $qvf.name 
+        Fileize             = $qvf.Filesize/1000
+        Reloaded            = $qvf.lastReloadTime
+    }
+    $s  
+    Export-QlikApp -id $qvf.id -filename "$($folder)\$($username)\$($qvf.name).qvf" 
+}
+
