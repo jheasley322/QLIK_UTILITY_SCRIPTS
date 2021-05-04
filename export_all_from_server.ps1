af63bc012d6ff7b6d62fb06a804947ef80016790
Joe Easley <40567905+jheasley322@users.noreply.github.com>
2021-05-04 06:59:14 -0400

    Update export_all_from_server.ps1
    finalize all export

--- export_all_from_server.ps1
+++ export_all_from_server.ps1
@@ -1,11 +1,32 @@
-Get-PfxCertificate "C:\Users\JoeEasley\OneDrive - Odyssey Logistics & Technology Corporation\Desktop\NC-JEASLY1\client.pfx" | Connect-Qlik -computername t1prd-qlik01.odysseylogistics.COM -username odysseylogistic\QlikServiceProd -TrustAllCerts -verbose
-$myserver = "t1prd-qlik01.odysseylogistics.COM"
-$folder = "C:\Users\JoeEasley\OneDrive - Odyssey Logistics & Technology Corporation\Desktop\Archive_Unused_5_3_21"
-$streamname = "UTIL - Out of Service"
+$certloc = "C:\Users\JoeEasley\OneDrive - Odyssey Logistics & Technology Corporation\Desktop\NC-JEASLY1\client.pfx"
+$myserver = "t1prd-qlik01.odysseylogistics.com"
+$archive = "Archive_Unused_5_3_21"
+$folder = "C:\Users\JoeEasley\OneDrive - Odyssey Logistics & Technology Corporation\Desktop\$archive"
+$username = "Joe Easley (NC)"
+$domainname = "ODYSSEYLOGISTIC"
+$serviceacct = "QlikServiceProd"
 
-Connect-Qlik $myserver ## check https://github.com/ahaydon/Qlik-Cli for details
 
-foreach($qvf in $(Get-QlikApp)) {
+Get-PfxCertificate "$certloc" | Connect-Qlik -computername "$myserver" -username "$domainname\$serviceacct" -TrustAllCerts -verbose
+
+##show list of apps to be exported
+[System.Collections.ArrayList]$apps = @()
+foreach($qvf in $(Get-QlikApp -full))
+{
+    $s = [PSCustomObject]@{
+        Name                = $qvf.name
+        Stream              = $qvf.stream.name 
+        Fileize_kb          = $qvf.Filesize/1000
+        Reloaded            = $qvf.lastReloadTime
+
+    }
+    $apps.Add($s)
+}
+$apps | Format-Table
+
+##export each app to the specified directory
+foreach($qvf in $(Get-QlikApp -full))
+{
     if ($qvf.published -and $qvf.stream.name) { # Is it published?
         $streamfolder = $qvf.stream.name
         If(!(test-path "$folder\$streamfolder")) # Create a folder if it does not exists
@@ -15,5 +36,12 @@ foreach($qvf in $(Get-QlikApp)) {
     } else {
         $streamfolder = ""
     }
-    Export-QlikApp -id $qvf.id -filename "$($folder)\$($streamfolder)\$($qvf.name).qvf" #dumps the qvf
+    $s = [PSCustomObject]@{
+        Name                = $qvf.name
+        Stream              = $qvf.stream.name  
+        Fileize             = $qvf.Filesize/1000
+        Reloaded            = $qvf.lastReloadTime
+    }
+    $s  
+    Export-QlikApp -id $qvf.id -filename "$($folder)\$($streamfolder)\$($qvf.name).qvf" -skipdata
 }
