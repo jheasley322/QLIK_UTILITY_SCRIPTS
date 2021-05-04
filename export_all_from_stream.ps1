6c127d96a05cdbcee398c7bccf15747b16703b16
Joe Easley <40567905+jheasley322@users.noreply.github.com>
2021-05-04 07:08:41 -0400

    Update export_all_from_stream.ps1
    finalize single stream export

--- export_all_from_stream.ps1
+++ export_all_from_stream.ps1
@@ -1,14 +1,13 @@
 $certloc = "C:\Users\JoeEasley\OneDrive - Odyssey Logistics & Technology Corporation\Desktop\NC-JEASLY1\client.pfx"
 $myserver = "t1prd-qlik01.odysseylogistics.com"
 $archive = "Archive_Unused_5_3_21"
-$streamname = "UTIL - Out of Service"
+$streamname = "CORP - IT Services"
 $folder = "C:\Users\JoeEasley\OneDrive - Odyssey Logistics & Technology Corporation\Desktop\$archive"
 $domainname = "ODYSSEYLOGISTIC"
 $serviceacct = "QlikServiceProd"
 
 Get-PfxCertificate "$certloc" | Connect-Qlik -computername "$myserver" -username "$domainname\$serviceacct" -TrustAllCerts -verbose
 Connect-Qlik $myserver ## check https://github.com/ahaydon/Qlik-Cli for details
-
 
 ##show list of apps to be exported
 [System.Collections.ArrayList]$apps = @()
@@ -24,7 +23,7 @@ foreach($qvf in $(Get-QlikApp -filter "stream.name eq '$streamname'" -full))
 $apps | Format-Table
 
 ##export each app to the specified directory
-foreach($qvf in $(Get-QlikApp -filter "stream.name eq '$streamname'")) {
+foreach($qvf in $(Get-QlikApp -filter "stream.name eq '$streamname'" -full)) {
     if ($qvf.published -and $qvf.stream.name) { # Is it published?
         $streamfolder = $qvf.stream.name
         If(!(test-path "$folder\$streamfolder")) # Create a folder if it does not exists
@@ -34,5 +33,10 @@ foreach($qvf in $(Get-QlikApp -filter "stream.name eq '$streamname'")) {
     } else {
         $streamfolder = ""
     }
+    $s = [PSCustomObject]@{
+        Name                = $qvf.name
+        Fileize_kb          = $qvf.Filesize/1000
+    }
+    $s  
     Export-QlikApp -id $qvf.id -filename "$($folder)\$($streamfolder)\$($qvf.name).qvf" 
 }
